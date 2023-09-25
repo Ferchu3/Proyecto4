e035c732d157375ca9926f71a8185ce296ab5ae2
CCB0\BACO <alangonzalez9407@gmail.com>
2023-09-25 15:39:42 -0600

    Final commit

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -5,22 +5,30 @@ import styles from './navbar.module.css'
 function Navbar(){
     const [selectedItem, setSelectedItem] = useState('Inicio');
 
-    const handleItemClick =(item) => {
+    const handleItemClick =(item, id) => {
         setSelectedItem(item);
-    }
+        scrollToSection(id);
+    };
+
+    const xcrollToSection =(id)=> {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({behavior: 'smooth'})
+        }
+    };
 }
 function Navbar( ) {
     return(
         <nav className={styles.navbar}>
             <div 
                 className={styles.navbarItem + ` ${selectedItem === 'Inicio' ? styles. selected : ''}`}
-                onClick={()=> handleItemClick('Inicio')}
+                onClick={()=> handleItemClick('Inicio', 'header')}
             >
                 <a href ="#header">Inicio</a>
             </div>
             <div 
                 className={styles.navbarItem + ` ${selectedItem === 'Skills' ? styles. selected : ''}`}
-                onClick={()=> handleItemClick('Skills')}
+                onClick={()=> handleItemClick('Skills', 'info')}
             >
                 <a href ="#info">Skills</a>
             </div>
