e1e35da2cd489be151021dd2500cb392552b7472
CCB0\BACO <alangonzalez9407@gmail.com>
2023-09-26 12:49:51 -0600

    Final Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from"./components/navbar"; 
+import Image from 'next/image'
+import styles from "./page.module.css"
+import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <main>
-      <navbar></navbar>
+      <Navbar></Navbar>
       <header id="header" className={styles.header}>
         <div>
           <h1>
             <span>
-              Hello<span className={styles.yellowText}>.</span>
+              Hello<span className={styles.yellowText}> </span>
             </span>
             <br />
             <span>
@@ -21,34 +21,34 @@ export default function Home() {
           </h1>
         </div>
       </header>
-      <section id="info" className={styles.infoSecion}>
+      <section id="info" className={styles.infoSection}>
         <img
-          src="/imagen-para-website.png"
-          alt="imagen"
-          className={styles.image}
+          src = "/imagen-para-website.png"
+          alt = "Imagen"
+          className = {styles.image}
         />
         <div className={styles.infoContainer}>
-          <span className={styles.title +" " +styles.yellowText}>Lionel</span>
+          <span className={styles.title + "" + styles.greenText}>Lionel</span>
           <br />
           <span className={styles.title}>Messi</span>
-        </div>
-        <div className={styles.list}>
-          <ul>
-            <li>
-              <span className={styles.grayText}> Age: </span> 15
-            </li>  
-            <li>
-              <span className={styles.grayText}> Nacionality: </span> Guatemaltecan
-            </li>
-            <li>
-              <span className={styles.grayText}> Skill set: </span> Project Management and Financial Performance
-            </li>
-            <li>
-              <span className={styles.grayText}> Languages: </span> Spanish, English
-            </li>
-          </ul>
+          <div className={styles.list}>
+            <ul>
+              <li>
+                <span className={styles.grayText}>Age: </span> 15
+              </li>
+              <li>
+                <span className={styles.grayText}>Nacionality: </span>Guatemaltecan
+              </li>
+              <li>
+                <span className={styles.grayText}>Skills: </span> Chambeador 
+              </li>
+              <li>
+                <span className={styles.grayText}>Languages: </span> English. Spanish
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     </main>
-  );
+  );   
 }
